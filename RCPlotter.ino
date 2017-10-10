e6edc71904f5bc24224830b332828df2772f4540
donovanmagryta <triops124@gmail.com>
2017-10-10 13:51:18 -0400

    Update RCPlotter.ino

--- RCPlotter.ino
+++ RCPlotter.ino
@@ -6,10 +6,10 @@
 
 #include <Servo.h> // pull in Servo library
 
-Servo servoA; // create servo object to control the elbow servo
-Servo servoB;  // create servo object to control the shoulder servo
-int potpinA = 0;  // A0 analog pin used to connect the elbow potentiometer
-int potpinB = 1; // A1 analog pin used to connect the shoulder potentiometer
+Servo servoA; // create servo object to control the left servo
+Servo servoB;  // create servo object to control the right servo
+int potpinA = 0;  // A0 analog pin used to connect the left potentiometer
+int potpinB = 1; // A1 analog pin used to connect the right potentiometer
 int valA; // variable to read the value from the first analog pin
 int valB; // variable to read the value from the second anaog pin
 
@@ -18,13 +18,13 @@ void setup() {  // setup runs only once on startup
   servoB.attach(10); // attached the servo on pin D10 to the servo object
 } //signify end of setup section
 
-void loop() { // loop runs over an over again
-  valA= analogRead(potpinA);  // reads the value of the elbow potentiometer (value between 0 and 1023)
+void loop() { // loop runs repeatedly
+  valA= analogRead(potpinA);  // reads the value of the left potentiometer (value between 0 and 1023)
   valA = map(valA, 0, 1023, 0, 180);  // scale it to use it with the servo (value between 0 and 180)
-  servoA.write(valA); // sets the elbow servo position according to the scaled value
+  servoA.write(valA); // sets the left servo position according to the scaled value
   delay(15);  // waits for the servo to get there
-  valB = analogRead(potpinB);  // reads the value of the shoulder potentiometer (value between 0 and 1023)
+  valB = analogRead(potpinB);  // reads the value of the right potentiometer (value between 0 and 1023)
   valB = map(valB, 0, 1023, 0, 180);  // scale it to use it with the servo (value between 0 and 180)
-  servoB.write(valB);  // sets the shoulder servo position according to the scaled value
+  servoB.write(valB);  // sets the right servo position according to the scaled value
   delay(15);  // waits for the servo to get there
 } //signify end of loop section
