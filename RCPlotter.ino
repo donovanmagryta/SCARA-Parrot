151ab9f0994fa306a7fa5e158c2eb5d2659990ad
donovanmagryta <triops124@gmail.com>
2017-10-10 13:36:21 -0400

    Update RCPlotter.ino

--- RCPlotter.ino
+++ RCPlotter.ino
@@ -4,27 +4,27 @@
 // small arm for control input, and a larger arm for moving a marker which mimics the small arm movement
 
 
-#include <Servo.h>
+#include <Servo.h> // pull in Servo library
 
-Servo servoelbow; // create servo object to control the elbow servo
-Servo servoshoulder;  // create servo object to control the shoulder servo
-int potpinelbow = 0;  // A0 analog pin used to connect the elbow potentiometer
-int potpinshoulder = 1; // A1 analog pin used to connect the shoulder potentiometer
-int val1; // variable to read the value from the first analog pin
-int val2; // variable to read the value from the second anaog pin
+Servo servoA; // create servo object to control the elbow servo
+Servo servoB;  // create servo object to control the shoulder servo
+int potpinA = 0;  // A0 analog pin used to connect the elbow potentiometer
+int potpinB = 1; // A1 analog pin used to connect the shoulder potentiometer
+int valA; // variable to read the value from the first analog pin
+int valB; // variable to read the value from the second anaog pin
 
 void setup() {  // setup runs only once on startup
-  servoelbow.attach(9); // attaches the servo on pin D9 to the servo object
-  servoshoulder.attach(10); // attached the servo on pin D10 to the servo object
-}
+  servoA.attach(9); // attaches the servo on pin D9 to the servo object
+  servoB.attach(10); // attached the servo on pin D10 to the servo object
+} //signify end of setup section
 
 void loop() { // loop runs over an over again
-  val1= analogRead(potpinelbow);  // reads the value of the elbow potentiometer (value between 0 and 1023)
-  val1 = map(val1, 0, 1023, 0, 180);  // scale it to use it with the servo (value between 0 and 180)
-  servoelbow.write(val1); // sets the elbow servo position according to the scaled value
+  valA= analogRead(potpinA);  // reads the value of the elbow potentiometer (value between 0 and 1023)
+  valA = map(valA, 0, 1023, 0, 180);  // scale it to use it with the servo (value between 0 and 180)
+  servoA.write(valA); // sets the elbow servo position according to the scaled value
   delay(15);  // waits for the servo to get there
-  val2 = analogRead(potpinshoulder);  // reads the value of the shoulder potentiometer (value between 0 and 1023)
-  val2 = map(val2, 0, 1023, 0, 180);  // scale it to use it with the servo (value between 0 and 180)
-  servoshoulder.write(val2);  // sets the shoulder servo position according to the scaled value
+  valB = analogRead(potpinB);  // reads the value of the shoulder potentiometer (value between 0 and 1023)
+  valB = map(valB, 0, 1023, 0, 180);  // scale it to use it with the servo (value between 0 and 180)
+  servoB.write(valB);  // sets the shoulder servo position according to the scaled value
   delay(15);  // waits for the servo to get there
-}
+} //signify end of loop section
